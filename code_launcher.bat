41547710fb79486a075a5ec07ab89310924a03d7
jamesibend <jamesbend86@gmail.com>
2017-09-23 21:10:18 +0100

    commit file via command line

---
+++ code_launcher.bat
@@ -0,0 +1,15 @@
+@echo off 
+START F:\"Sublime Text 3"\sublime_text.exe
+START C:\Users\James\AppData\Local\Programs\Python\Python36\Lib\idlelib\idle.pyw
+START C:\WINDOWS\system32\cmd.exe
+START C:\Windows\explorer.exe
+[general]
+v292_msg=0
+show_walkthru=0
+gui_x=732
+gui_y=293
+last_tab=
+filename=code_launcher.bat
+is_reloading=0
+version=2.94
+win_ver=10.0.15063
